d222822b72bfa6a3ce43d9443c08e811837a306b
Padhilam Tejasri <119599856+tejasri01@users.noreply.github.com>
2026-02-17 12:13:11 +0530

    Update user_data.sh

--- user_data.sh
+++ user_data.sh
@@ -16,7 +16,7 @@ cat <<EOF > /var/www/html/index.html
   <title>Hello World</title>
 </head>
 <body style="text-align:center; margin-top:100px;">
-  <h1>Hello from Buddi</h1>
+  <h1>This is my first Terraform project</h1>
   <p>Deployed using Terraform User Data</p>
 </body>
 </html>
